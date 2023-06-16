d587a8c755c26b88e1f6cc4e7435974522909934
Stefano Mandelli <linuxfree2@gmail.com>
2023-06-16 18:15:38 +0200

    Removed some junk lines

--- quaternions.cpp
+++ quaternions.cpp
@@ -1,9 +1,3 @@
-
-// Copyright (c) 2015-2020 by the parties listed in the AUTHORS file.
-// All rights reserved.  Use of this source code is governed by
-// a BSD-style license that can be found in the LICENSE file.
-
-
 #include "sys_utils.h"
 #include "math_sf.h"
 #include "quaternions.h"
